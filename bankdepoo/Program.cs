2935919e390408cb76c3606d9f5de7c4216d6d2f
BabakAdam <babaka@kkszki.hu>
2024-10-11 12:00:23 +0200

    :3

--- bankdepoo/Program.cs
+++ bankdepoo/Program.cs
@@ -10,6 +10,17 @@ namespace bankdepoo
     {
         static void Main(string[] args)
         {
+            Console.Write("Hány évre szeretnéd lekötni a pénzt? ");
+            int years = int.Parse(Console.ReadLine());
+
+            Console.Write("Mekkora összeget szeretnél lekötni? ");
+            double principal = double.Parse(Console.ReadLine());
+
+            Console.Write("Mi a lekötés kamata (%-ban)? ");
+            double interestRate = double.Parse(Console.ReadLine());
+
+            double totalAmount = principal * Math.Pow(1 + (interestRate / 100), years);
+            Console.WriteLine($"A lekötés végén a teljes összeg: {totalAmount:F2} Ft");
         }
     }
 }
